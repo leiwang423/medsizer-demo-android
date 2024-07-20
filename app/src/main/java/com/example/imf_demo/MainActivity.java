0e7f70a58940675095287c4780c7c7b24ac53980
leiwang <7508666@qq.com>
2024-07-20 17:36:54 +0800

    Upgrade com.mikepenz:iconics to 4.x. Support SDK version 31

--- app/src/main/java/com/example/imf_demo/MainActivity.java
+++ app/src/main/java/com/example/imf_demo/MainActivity.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.mikepenz.iconics.Iconics;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
